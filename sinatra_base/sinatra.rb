c9ce9802c9e8047e3660b8dafe170458a068282b
dickyince <minnimince@googlemail.com>
2015-01-07 23:27:29 +0000

    Starting work on basic auth framework

--- sinatra_base/sinatra.rb
+++ sinatra_base/sinatra.rb
@@ -8,6 +8,20 @@ require './lib/dao'
 
 set :bind, '0.0.0.0'
 
+enable :sessions
+
+userTable = {}
+
+helpers do
+  def login?
+    !!session[:username].nil
+  end
+
+  def username
+    return session[:username]
+  end
+end
+
 get "/" do
   @title = 'Home Page'
   erb :home
@@ -21,6 +35,33 @@ end
 get '/contact' do
   @title = 'Contact'
   erb :contact
+end
+
+post '/login' do
+  if userTable.has_key?(params[:username])
+    user = userTable[params[:username]]
+    if user[:passwordhash] == BCrypt::Engine.hash_secret(params[:password], user[:salt])
+      session[:username] = params[:username]
+    end
+  end
+  erb :home
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  password_salt = BCrypt::Engine.generate_salt
+  password_hash = BCrypt::Engine.hash_secret(params[:password], password_salt)
+
+  #ideally this would be saved into a database, hash used just for sample
+  userTable[params[:username]] = {
+    :salt => password_salt,
+    :passwordhash => password_hash
+  }
+
+  redirect "/"
 end
 
 get "/styles.css" do
